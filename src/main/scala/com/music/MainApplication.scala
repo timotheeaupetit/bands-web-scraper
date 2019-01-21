6b6681af1d02afdfca13ca5705975f2de62126a7
Timothée AUPETIT <timotheeaupetit@ppc-mbp-1709.local>
2019-01-21 16:47:48 +0100

    Managed to load a set of band names from a file which path is given as an Environment Variable. Deleted Resources file.

--- src/main/scala/com/music/MainApplication.scala
+++ src/main/scala/com/music/MainApplication.scala
@@ -3,15 +3,25 @@ package com.music
 import com.music.utils.ProjectConfiguration
 import com.music.utils.ProjectConfiguration._
 
+import scala.io.Source
+import scala.util.{Failure, Success, Try}
+
 object MainApplication extends App {
   ProjectConfiguration
     .projectConfiguration()
     .fold(
       Launcher.showConfigError, { configuration: ProjectConfig =>
-        println(configuration.appConfig.application)
         val processor = Processor(configuration.appConfig.base_url, configuration.appConfig.api_url)
-        processor.process()
+        val artists = fetchArtists(configuration.appConfig.input_file)
+
+        processor.process(artists)
       }
     )
 
+  final private def fetchArtists(path: String): Set[String] = Try(Source.fromFile(path)) match {
+    case Success(src) => src.getLines.map(_.toLowerCase).toSet
+    case Failure(_) =>
+      println("Could not open file located at " + path)
+      Set.empty[String]
+  }
 }
